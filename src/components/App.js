b0f81efc71e24137cbe35cd86e7855d78894789b
fable0831 <fable0831@gmail.com>
2023-03-08 23:52:00 +0900

    fbase setting

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
+import { useState } from "react";
+import AppRouter from "components/Router";
+import { authService } from "./fbase";
+
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
     <>
-      <h1>Hello</h1>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Fwitter</footer>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "components/App";
+import app from "components/fbase";
+console.log(app);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
